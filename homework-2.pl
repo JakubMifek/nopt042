6c0164bcb990c04f9f59b5b339b49e8b6b9ab735
Jakub Mifek <jakubmifek@gmail.com>
2019-10-15 17:33:15 +0200

    Update homework-2.pl

--- homework-2.pl
+++ homework-2.pl
@@ -3,4 +3,4 @@ fib(N, N, _, X, X).
 fib(N, M, A, B, X):-N > M, M2 is M+1, C is A+B, fib(N, M2, B, C, X).
 
 fibonacci(0, 1).
-fibonacci(N, X):-N > 0, fib(N, 0, 1, 1, X).
+fibonacci(N, X):-N > 0, fib(N, 0, 0, 1, X).
