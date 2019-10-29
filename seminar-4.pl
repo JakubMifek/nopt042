d70b7cd5b91fc2f3a5f9bbdea682f6f291e5b21b
Jakub Mifek <jakubmifek@gmail.com>
2019-10-29 13:55:56 +0100

    HW progress

--- seminar-4.pl
+++ seminar-4.pl
@@ -54,38 +54,80 @@ solve_knapsack(Capacity, Weights, Prices, Sol, P):-
     labeling([maximize(P)], Vars).
 
 %house move
+
+% makes each member equal to the given value
+each_member([], _):-!.
+each_member([H|T], Val):-
+	H #= Val,
+	each_member(T, Val).
+	
+% creates an empty array of given length
+empty_array(N, Arr):-
+	length(Arr, N),
+	each_member(Arr, 0).
+
+% creates domains for schedules
 make_schedule_domains([], [], _):-!.
 make_schedule_domains([SH | ST], [TH | TT], Time):-
     X is Time-TH,
     domain([SH], 0, X),
     make_schedule_domains(ST, TT, Time).
 
-sum_people(_, _, [], [], [], 0).
-sum_people(Start, End, [SH|ST], [TH|TT], [PH|PT], People):-
-    ((Start #< SH #/\ End #> SH + TH) #=> P #= PH) #\/ P #= 0,
-    sum_people(Start, End, ST, TT, PT, PS),
-    People #= P + PS.
+% Creates allocation subarrays
+create_sub_arrays([], _, _):-!.
+create_sub_arrays([H|T], Time, People):-
+	length(H, Time),
+	domain(H, 0, People),
+	create_sub_arrays(T, Time, People).
 
-assert_people_at_times([], [], [], _):-!.
-assert_people_at_times([Start|ST], [Takes|TT], [PH|PT], People):-
-    End #= SH + Takes
-    sum_people(Start, End, ST, TT, PT, PS),
-    P #= PS + PH,
-    P #< People,
-    assert_people_at_times(ST, TT, PT, People).
+% Fills allocations for an item. Allocation in time T is 0 if the item is not moving or Need if the item is moving.
+fill_allocations([], _, _, _, _):-!.
+fill_allocations([AH|AT], Idx, Start, End, Need):-
+	((Idx #< Start #\/ Idx #> End) #=> AH #= Need),
+	((Idx #>= Start #/\ Idx #=< End) #=> AH #= 0),
+	Idx2 is Idx+1,
+	fill_allocations(AT, Idx2, Start, End, Need).
 
+% Creates conditions for allocations for all items
+assert_people_at_times([], [], [], []):-!.
+assert_people_at_times([Start|ST], [A|AT], [Takes|TT], [PH|PT]):-
+    End #= Start + Takes,
+	fill_allocations(A, 0, Start, End, PH),
+	assert_people_at_times(ST, AT, TT, PT).
+
+% Adds two lists together and returns the result. Adding is done via conditions.
+add_lists([], [], []):-!.
+add_lists([H1|T1], [H2|T2], [RH|RT]):-
+	RH #= H1 + H2,
+	add_lists(T1, T2, RT).
+	
+% Sums 2D array by columns using conditions.
+assert_sum_each([A|[]], A):-!.
+assert_sum_each([AH1, AH2 | AT], Res2):-
+	add_lists(AH1, AH2, Res),
+	assert_sum_each([Res|AT], Res2).
+	
+% Asserts that all allocations are consistent.
+assert_sum(Allocations, People, Res):-
+	assert_sum_each(Allocations, Res),
+	domain(Res, 0, People).
+	
+% Solves the moving problem.
 solve_move(Time, People, TimeNeeded, PeopleNeeded, Schedule):-
     % Declarations
     length(TimeNeeded, N), length(PeopleNeeded, N), length(Schedule, N),
-    length(Allocations, Time),
+	length(Allocations, N),
+	create_sub_arrays(Allocations, Time, People),
 
     % Make domains
     make_schedule_domains(Schedule, TimeNeeded, Time),
-    domain(Allocations, 0, People),
-
+	
     % Conditions
     % Sum of people at a time must be lower or equal to people
-    assert_people_at_times(Schedule, TimeNeeded, PeopleNeeded, People).
+    assert_people_at_times(Schedule, Allocations, TimeNeeded, PeopleNeeded),
+	assert_sum(Allocations, People, Res),
 
+	append(Res, Schedule, Vars),
+	
     % Solution
-    labeling([], Schedule).
+    labeling([], Vars).
